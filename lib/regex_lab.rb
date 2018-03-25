0a070b86c253bfcee5e12bf2be27a9e32a17fef2
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-25 17:02:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  return text.scan(/^[A-Z].*\.$/) ? true : false
+  r = text.scan(/^[A-Z].*\.$/)
+
+  binding.pry
 end
 
 def valid_phone_number?(phone)
