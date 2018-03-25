f70168233aa75be006839954a49f0f47bc4dc293
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-25 16:44:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/\bun\S*ing\b/i)
+  text.match(/\bun\S*ing\b/i).to_a
 end
 
 def words_five_letters_long(text)
